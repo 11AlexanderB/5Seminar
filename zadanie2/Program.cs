541a0ed019f0ac024f1d4240bd62b4e3235ec9f0
11AlexanderB <bezhenar93@internet.ru>
2022-09-03 23:26:52 +0300

    Sum

---
+++ zadanie2/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] FillArray()
+{
+    Console.WriteLine("Количество элементов массива: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    int [] Array = new int[N];
+
+    for( int i = 0; i<Array.Length; i++ )
+    {
+        Array[i] = new Random().Next (-10, 10);
+        if(i < Array.Length-1) Console.Write($" {Array[i]}, ");
+        else Console.Write($" {Array[i]}  ");
+    }
+    return Array;
+}
+int[] mda = FillArray();
+int SumOdd(int [] Array)
+{
+    int Sum = 0;
+    
+    for( int i = 1; i < Array.Length; i= i+2) Sum = Sum+Array[i];
+    return Sum;
+}
+int results = SumOdd(mda);
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях равно: {results}");
