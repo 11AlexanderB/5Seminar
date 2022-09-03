387c7a728a9daebc2c436d71c31eb86dfc4ae198
11AlexanderB <bezhenar93@internet.ru>
2022-09-03 23:32:52 +0300

    difference

---
+++ zadanie3/Program.cs
@@ -0,0 +1,34 @@
+﻿double [] FillArray()
+{
+    Console.WriteLine("Размер массива равен: ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    double[] Array = new double[N];
+
+    for( int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = Math.Round( new Random().Next(-10, 10) + new Random().NextDouble(), 2 );
+        if( i < Array.Length-1 ) Console.Write($"{Array[i]};  ");
+        else Console.Write($"{Array[i]}  ");
+    }
+    return  Array;
+}
+double [] mda = FillArray();
+double difference(double[] Array)
+{
+    double min = Array[0];
+    double max = Array[0];
+    
+    for( int i = 0; i < Array.Length; i++)
+    {
+        if( Array[i] > max) max = Array[i];
+    }
+    for( int i = 0; i < Array.Length; i++)
+    {
+        if( Array[i] < min) min = Array[i];
+    }
+    double def = max - min;
+    return def;
+}
+double results =  difference(mda);
+Console.WriteLine();
+Console.WriteLine($" Разница между макс. и мин. элементов массива: {results}");
